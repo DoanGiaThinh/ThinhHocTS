3d855706ae7a5f9699bb38e2f7fe35d7f5d4b4ff
Thinh Hoc JS <bonemqst@gmail.com>
2026-04-13 20:33:21 +0700

    lesson23

---
+++ chapter3/backup-chapter3.ts
@@ -0,0 +1,70 @@
+// //lesson 20
+// let age: number = 0;
+
+// if (age) { // number > 0 || string "ádasdasdasd"
+//     console.log("Welcome to the university")
+// }
+// else { //number = 0 || string " "
+//     console.log("NOOOOOOOOOOOOOOOOOOOOOOOOOOOOO")
+// }
+
+// let discount: number;
+// let itemCount = 11;
+
+// if (itemCount > 0 && itemCount < 5) {
+//     discount = 5;
+// }
+// else if (itemCount >= 5 && itemCount < 10) {
+//     discount = 10;
+// }
+// else {
+//     discount = 15;
+// }
+
+// console.log("check discount", discount)
+// //lesson21
+// let age1: number = 25
+
+// switch (age1) {
+//     case 25:// mô hình tháp nước
+//     case 24:
+//         console.log("Đã đi làm")
+//         break;
+//     case 20:
+//         console.log("is the student")
+//         break;
+//     default:
+//         console.log("run default")
+// }
+//lesson 22
+
+// let count: number = 10;
+
+// for (let i = 0; i < count; i++) {
+//     console.log("Check count i", i);
+// }
+// // không nên viết như ở dưới
+// let i1: number = 0;
+// for (; i1 < count; i1++) {
+//     console.log("Check count i", i1);
+// }
+
+// for (let i2 = 0; ; i2++) {
+//     console.log("Check count i", i1);
+//     if (i2 > 9) break;
+// }
+//lesson 23
+// let count: number = 0;
+
+// while (count < 5) {
+//     console.log("Count ", count);
+//     count++;
+// }
+
+// let count1: number = 0;
+
+// while (count1 < 5) {
+//     console.log("Count1 ", count1);
+//     if (count1 % 2 === 0) break;
+//     count1++;
+// }
